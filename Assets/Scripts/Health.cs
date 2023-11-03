0cbc674e65ec1c972a284be7e21657d2565795d0
Metin Pişkin <85956297+Metin-Piskin@users.noreply.github.com>
2023-11-03 16:38:58 +0300

    PlatformerGame

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+   
+    public void TakeDamage(int damage)
+    {
+        health = health - damage;
+        CheckIfWeDead();
+    }
+
+    private void CheckIfWeDead()
+    {
+        if (health <= 0)
+        {
+            health = 0;
+            Debug.Log("We are dead" + gameObject.name);
+        }
+    }
+}
